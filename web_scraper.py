54d530d34814d0b6ca607d275f3586120ff7cbff
Jonathan Schwarz <schwarz.jonathan@icloud.com>
2023-03-16 17:11:14 +0200

    request bug

--- web_scraper.py
+++ web_scraper.py
@@ -1,10 +1,17 @@
+from gevent import monkey
+monkey.patch_all()
 import requests
 import grequests
-import bs4
+from bs4 import BeautifulSoup
+from fake_useragent import UserAgent
 
 
 def main():
-    pass
+    URL = 'https://www.nasdaq.com/news-and-insights/topic/markets'
+    r = requests.get(URL).text
+    soup = BeautifulSoup(r, 'html.parser')
+    pages = [f"https://www.nasdaq.com{a['href']}" for a in soup.find_all('a', class_="content-feed__card-title-link")]
+    print(pages)
 
 
 if __name__ == "__main__":
