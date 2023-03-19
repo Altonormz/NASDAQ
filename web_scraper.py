c7227e6c58ae292fb4e6ead2dfe3cf9d7200536c
Jonathan Schwarz <schwarz.jonathan@icloud.com>
2023-03-19 08:32:54 +0200

    gather articles urls

--- web_scraper.py
+++ web_scraper.py
@@ -1,17 +1,42 @@
 from gevent import monkey
 monkey.patch_all()
-import requests
 import grequests
 from bs4 import BeautifulSoup
-from fake_useragent import UserAgent
+
+
+def scrape_page(URL):
+    """
+    gathers articles urls from a NASDAQ articles web page
+    :param URL: response from server
+    :return: articles urls (beside the last one which is empty)
+    """
+    soup = BeautifulSoup(URL.text, 'html.parser')
+    pages = [f"https://www.nasdaq.com{a['href']}" for a in soup.find_all('a', class_="content-feed__card-title-link")]
+    return pages[:len(pages) - 1]
+
+
+def get_response(urls):
+    """
+    using grequests threads to get responses from several web pages at a time
+    :param urls: list of urls
+    :return: responses from server
+    """
+    headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                             'Chrome/75.0.3770.142 Safari/537.36'}
+    request = [grequests.get(url, headers=headers) for url in urls]
+    responses = grequests.map(request)
+    return responses
 
 
 def main():
-    URL = 'https://www.nasdaq.com/news-and-insights/topic/markets'
-    r = requests.get(URL).text
-    soup = BeautifulSoup(r, 'html.parser')
-    pages = [f"https://www.nasdaq.com{a['href']}" for a in soup.find_all('a', class_="content-feed__card-title-link")]
-    print(pages)
+    new_links = []
+    for i in range(1, 1000, 10):
+        ten_pages = [f'https://www.nasdaq.com/news-and-insights/topic/markets/page/{j}' for j in
+                     range(i, i+10)]
+        responses = get_response(ten_pages)
+        for url in responses:
+            new_links = new_links + scrape_page(url)
+    print(new_links)
 
 
 if __name__ == "__main__":
