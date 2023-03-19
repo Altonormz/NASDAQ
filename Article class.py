0ebe77d2cda997d67ef2986adc9d839e6926dd65
Altonormz <Altonormz@gmail.com>
2023-03-19 14:28:02 +0200

    "Removed soup from the class functions, so the memory of each object will be reduced."

--- Article class.py
+++ Article class.py
@@ -2,7 +2,6 @@ from gevent import monkey
 
 monkey.patch_all()
 import os
-import random
 import grequests
 from bs4 import BeautifulSoup
 import pandas as pd
@@ -21,58 +20,51 @@ class Article:
 
     def __init__(self, id_num, url):
         self.title = None
-        self._soup = None
         self.authors = None
         self.date = None
         self.tags = None
         self.id_num = id_num
         self.url = url
 
-    def _get_soup(self, response):
-        """
-        Parses the HTML content and returns a soup object.
-        """
-        return BeautifulSoup(response.content, "html.parser")
-
-    def _get_title(self):
+    def _get_title(self, soup):
         """
         Returns the name of the title of the article from the soup object.
         """
-        title_block = self._soup.find('h1', {'class': "jupiter22-c-hero-article-title"})
+        title_block = soup.find('h1', {'class': "jupiter22-c-hero-article-title"})
         if title_block:
             title = title_block.get_text().strip()
             return title
         else:
             return None
 
-    def _get_authors(self):
+    def _get_authors(self, soup):
         """
         Returns the names of the authors of the article from the soup object.
         """
-        authors_block = self._soup.find('span', {"class": "jupiter22-c-author-byline__author-no-link"})
+        authors_block = soup.find('span', {"class": "jupiter22-c-author-byline__author-no-link"})
         if authors_block:
             authors = authors_block.get_text().strip()
         else:
             authors = "Unknown"
         return authors
 
-    def _get_date(self):
+    def _get_date(self, soup):
         """
         Returns the date of the article from the soup object.
         """
-        time_stamp_block = self._soup.find('p', {"class": "jupiter22-c-author-byline__timestamp"})
+        time_stamp_block = soup.find('p', {"class": "jupiter22-c-author-byline__timestamp"})
         if time_stamp_block:
             time_stamp = time_stamp_block.get_text().strip()
         else:
             time_stamp = "Unknown"
         return time_stamp
 
-    def _get_tags(self):  # Fix
+    def _get_tags(self, soup):  # Fix
 
         """
         Returns the tags of the article from the soup object.
         """
-        tags_block = self._soup.find('div', {"class": "jupiter22-c-tags-container"})
+        tags_block = soup.find('div', {"class": "jupiter22-c-tags-container"})
         if tags_block:
             tags_list = tags_block.get_text(" ").split()
         else:
@@ -80,22 +72,22 @@ class Article:
         return tags_list
 
     def set_info(self, response):
-        self._soup = self._get_soup(response)
-        self.title = self._get_title()
-        self.authors = self._get_authors()
-        self.date = self._get_date()
-        self.tags = self._get_tags()
-        self._create_article_content_file()
+        soup = get_soup(response)
+        self.title = self._get_title(soup)
+        self.authors = self._get_authors(soup)
+        self.date = self._get_date(soup)
+        self.tags = self._get_tags(soup)
+        self._create_article_content_file(soup)
 
-    def _create_article_content_file(self):
+    def _create_article_content_file(self, soup):
         """
-        Creates a text file containing the article content in a subfolder
+        Creates a text file containing the article content in a sub folder.
         """
         if not os.path.isdir("article content files"):
             os.mkdir("article content files")
         try:
             with open(os.path.join("article content files", f'{self.id_num}.txt'), 'w') as f:
-                paragraphs = self._soup.find_all('p')
+                paragraphs = soup.find_all('p')
                 content = "\n".join([p.get_text() for p in paragraphs])
                 f.write(content)
         except:
@@ -116,6 +108,13 @@ class Article:
                 'tags': self.tags,
                 'URL': self.url}
         return data
+
+
+def get_soup(response):
+    """
+    Parses the HTML content and returns a soup object.
+    """
+    return BeautifulSoup(response.content, "html.parser")
 
 
 def setting_info(article_list):
