cb8fbc36c9b65db1edaf45caaf7be83ee9cbaba5
Altonormz <Altonormz@gmail.com>
2023-04-05 15:43:11 +0300

    "NASDAQ_datacollector works! It works! Things to note: Need to ask user for details on localhost, user, password And so much hardcoded Changed NASDAQ.sql to NASDAQ_sql_schema.sql.sql"

---
+++ NASDAQ_sql_schema.sql
@@ -0,0 +1,35 @@
+CREATE TABLE IF NOT EXISTS `Articles` (
+  `article_id` INTEGER PRIMARY KEY AUTO_INCREMENT,
+  `author_id` int,
+  `title` varchar(255),
+  `article_content` varchar(255),
+  `url` varchar(255),
+  `published_date` datetime
+);
+
+CREATE TABLE IF NOT EXISTS `Authors` (
+  `author_id` INTEGER PRIMARY KEY AUTO_INCREMENT,
+  `author_name` varchar(255)
+);
+
+CREATE TABLE IF NOT EXISTS `Tags` (
+  `tag_id` INTEGER PRIMARY KEY AUTO_INCREMENT,
+  `tag_name` varchar(255)
+);
+
+CREATE TABLE IF NOT EXISTS `Article_Tags` (
+  `article_tag_id` INTEGER PRIMARY KEY AUTO_INCREMENT,
+  `article_id` int,
+  `tag_id` int
+);
+
+CREATE TABLE IF NOT EXISTS `Stocks` (
+  `stock_id` INTEGER PRIMARY KEY AUTO_INCREMENT,
+  `stock_tick` varchar(255)
+);
+
+CREATE TABLE IF NOT EXISTS `Stock_Articles` (
+  `stock_article_id` INTEGER PRIMARY KEY AUTO_INCREMENT,
+  `stock_id` int,
+  `article_id` int
+);
