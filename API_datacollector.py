df80e24f928e64536017d365ddf7cb86ad6f4b3f
Altonormz <Altonormz@gmail.com>
2023-04-24 23:45:28 +0300

    Finished the API_datacollector.py Added logging and documentation.

--- API_datacollector.py
+++ API_datacollector.py
@@ -3,9 +3,7 @@ import json
 import requests
 import pandas as pd
 import pymysql.cursors
-import dateparser
 import time
-import datetime
 
 with open("conf.json") as f:
     config = json.load(f)
@@ -17,6 +15,9 @@ logger = logging.getLogger("NASDAQ_scraper.log")
 
 
 def start_sql_connection():
+    """
+    Returns connection to MySQL, using the user and password from the config file.
+    """
     connection = pymysql.connect(host='localhost',
                                  user=config["USER"],
                                  password=config["PASSWORD"],
@@ -26,40 +27,50 @@ def start_sql_connection():
 
 
 def call_info_stocks_api(ticker, info_token_flag):
+    """
+    Returns DataFrame containing the information about the ticker.
+    Returns a boolean flag representing if the queries are spent from the token.
+    """
     tick_info = []
     info_token = config['API']['info_token']
     api_url = f"https://www.alphavantage.co/query?function=OVERVIEW&symbol={ticker}&apikey={info_token}"
     response = requests.get(api_url)
+    response = response.json()
+
     if 'Note' in response:
         info_token_flag = False
     if info_token_flag:
-        tick_info.append(response.json())
-
+        tick_info.append(response)
     tick_info_df = pd.DataFrame(tick_info)
     return tick_info_df, info_token_flag
 
 
 def call_price_stocks_api(ticker, price_token_flag):
+    """
+    Returns DataFrame containing the information about the ticker prices.
+    Returns a boolean flag representing if the queries are spent from the token.
+    """
     tick_prices_df = pd.DataFrame({})
     price_token = config['API']['price_token']
     api_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={ticker}&outputsize" \
               f"=compact&apikey={price_token}"
     response = requests.get(api_url)
+    response = response.json()
     if 'Note' in response:
         price_token_flag = False
     if price_token_flag:
-        response = response.json()
+
         if response:
             tick_prices_df = pd.DataFrame(response["Time Series (Daily)"]).T
 
     return tick_prices_df, price_token_flag
-#
-# response = requests.get(api_url)
-#         response = response.json()
-#         prices = pd.DataFrame(response["Time Series (Daily)"]).T
+
 
 
 def update_stocks(tick_info, connection):
+    """
+    Gets a DataFrame containing the ticker information, and updates the Stocks Table using the information.
+    """
     for i, row in tick_info.iterrows():
         cursor = connection.cursor()
         cursor.execute(f'UPDATE Stocks SET name = %s, currency = %s, country = %s, sector = %s, industry = %s '
@@ -69,6 +80,9 @@ def update_stocks(tick_info, connection):
 
 
 def insert_stock_prices(prices, ticker, connection):
+    """
+    Gets a DataFrame containing the ticker prices and name, and updates the Stocks Table using the information.
+    """
     cursor = connection.cursor()
     cursor.execute('SELECT stock_id FROM Stocks where stock_tick = %s', (ticker,))
     stock_id = cursor.fetchone()['stock_id']
@@ -79,16 +93,35 @@ def insert_stock_prices(prices, ticker, connection):
         connection.commit()
 
 
-def update_stock_prices(connection):
+def update_stock_prices():
+    """
+    Updates the Table Stocks_Prices, with the most recent available prices without duplicates.
+    """
+    connection = start_sql_connection()
     cursor = connection.cursor()
-    cursor.execute("SELECT j.stock_tick, MAX(j.date) FROM ("
+    # Query to get the last date the price was updated with the ticker name.
+    cursor.execute("SELECT jj.stock_tick as stock_tick, jj.MAX_DATE as last_date FROM ("
+                   "SELECT j.stock_tick as stock_tick, DATE(MAX(j.date)) as MAX_DATE FROM ("
                    "SELECT stock_tick, date FROM Stocks_Prices LEFT JOIN Stocks ON "
-                   "Stocks_Prices.stock_id = Stocks.stock_id) as j GROUP BY j.stock_tick ORDER BY MAX(date) DESC")
+                   "Stocks_Prices.stock_id = Stocks.stock_id) as j GROUP BY j.stock_tick ORDER BY MAX(date)) as jj "
+                   "WHERE jj.MAX_DATE < CURDATE()")
     last_prices = pd.DataFrame(cursor.fetchall())
-    print(last_prices)
+    price_token_flag = True
+    for i, ticker in enumerate(last_prices['stock_tick']):
+        if price_token_flag:
+            tick_prices_df, price_token_flag = call_price_stocks_api(ticker, price_token_flag)
+            if not tick_prices_df.empty:
+                last_prices['last_date'] = pd.to_datetime(last_prices['last_date'])
+                tick_prices_df.index = pd.to_datetime(tick_prices_df.index)
+                tick_prices_df_new = tick_prices_df[tick_prices_df.index > last_prices['last_date'][i]]
+                insert_stock_prices(tick_prices_df_new, ticker, connection)
+                logging.info(f' "{ticker}" was updated in the Stocks_Prices table. ')
 
 
 def new_tickers():
+    """
+    Adds the tickers Information and stock prices to DataBase for stocks with no information.
+    """
     connection = start_sql_connection()
     cursor = connection.cursor()
     cursor.execute('SELECT stock_tick FROM Stocks where name IS NULL')
@@ -98,37 +131,23 @@ def new_tickers():
         info_token_flag = True
         if i > config['API']['query_limit']:
             break
-        if (i > 0) and (i % 3 == 0):
+        if (i > 0) and (i % 2 == 0):
             time.sleep(65)
 
         if info_token_flag:
             tick_info_df, info_token_flag = call_info_stocks_api(ticker, info_token_flag)
             if not tick_info_df.empty:
                 update_stocks(tick_info_df, connection)
+                logging.info(f' "{ticker}" was updated in the Stocks table. ')
 
         price_token_flag = True
         if price_token_flag:
             tick_prices_df, price_token_flag = call_price_stocks_api(ticker, price_token_flag)
             if not tick_prices_df.empty:
                 insert_stock_prices(tick_prices_df, ticker, connection)
-
-        connection.commit()
-
-
-
-def emp():
-
-    q = """
-    UPDATE Stocks SET name = null, currency = null, country = null, sector = null, industry = null where stock_tick = 'SNY';
-    """
-    connection = start_sql_connection()
-    cursor = connection.cursor()
-    cursor.execute(q)
-    connection.commit()
+                logging.info(f' "{ticker}" was updated in the Stocks_Prices table. ')
 
 
 
-new_tickers()
-# emp()
 
 
